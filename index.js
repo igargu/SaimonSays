e70817c33a4b4cdd8a5b2e6621e85532b00a07c3
Iker-hub <ikergargu@gmail.com>
2023-06-26 21:30:36 +0200

    Working on the js

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
+let counter = 0;
+let succession;
+const colorSuccession = [];
+
 let controlButton = document.getElementById("control-button");
 
-let counter = 0;
-const colorSuccession = [];
+document.getElementById("color-1").addEventListener("click", function () {
+  checkColor(1);
+});
+document.getElementById("color-2").addEventListener("click", function () {
+  checkColor(2);
+});
+document.getElementById("color-3").addEventListener("click", function () {
+  checkColor(3);
+});
+document.getElementById("color-4").addEventListener("click", function () {
+  checkColor(4);
+});
 
 controlButton.addEventListener("click", function () {
   gameStart();
 });
 
 function gameStart() {
-  controlButton.style.display = "none";
-  // Disabled color buttons
-  colorSuccession.forEach(showColor(value));
+  succession = 0;
+  controlButton.style.visibility = "hidden";
+  changeColorButtonsState(false);
+  colorSuccession.forEach(function (value) {
+    showColor(value);
+  });
   var newColor = Math.floor(Math.random() * 4 + 1);
   colorSuccession.push(newColor);
   showColor(newColor);
-  // Abled color buttons
+  console.log(succession);
+  changeColorButtonsState(true);
 }
 
 function checkColor(idColor) {
-  if (idColor === colorSuccession[counter]) {
+  if (idColor === colorSuccession[succession]) {
     showColor(idColor);
-    incrementCounter();
+    succession++;
   } else {
     gameOver();
   }
-  if (counter === colorSuccession.length + 1) {
+  if (succession === colorSuccession.length) {
+    incrementCounter();
     gameStart();
   }
 }
 
 function showColor(idColor) {
   var color = document.getElementById(`color-${idColor}`);
-  // Add hover
-  // Wait 3 segs
-  // Remove hover
+  color.classList.add("active");
+  setTimeout(function () {
+    color.classList.remove("active");
+  }, 3000);
 }
 
 function incrementCounter() {
@@ -41,11 +61,18 @@ function incrementCounter() {
   document.getElementById("counter").innerHTML = counter;
 }
 
+function changeColorButtonsState(state) {
+  for (var i = 1; i <= 4; i++) {
+    document.getElementById(`color-${i}`).onclick = state;
+  }
+}
+
 function gameOver() {
+  console.log("You lose");
   // Show modal: "You lose! Record: ${counter}"
   // Disabled DOM under modal
   // When close modal {
   //    Restart counter
   //    Empty array
-  controlButton.style.display = "block";
+  controlButton.style.visibility = "visible";
 }
